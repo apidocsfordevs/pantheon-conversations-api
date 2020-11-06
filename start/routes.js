268d06846e74d67f4023e175363af5fbf6772c8a
shalvah <diakon.ng@gmail.com>
2020-11-06 03:28:07 +0100

    Add Dredd for APi testing

--- start/routes.js
+++ start/routes.js
@@ -5,3 +5,15 @@ const Route = use('Route')
 Route.get('/', () => {
     return {hello: 'world'};
 });
+
+Route.post('/startConversation', async ({request}) => {
+    const Database = use('Database')
+    const conversationId = await Database
+        .table('conversations')
+        .insert({
+            app_id: request.input('on'),
+            with_user_id: request.input('with'),
+            by_user_id: 34,
+        });
+    return {conversationId: `${conversationId}`};
+})
